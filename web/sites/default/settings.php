f6e320ab793fde921055844ddb23ac30cebe634f
Evan Cunningham <ecunningham@sd38.bc.ca>
2022-08-02 16:32:03 -0700

    updating setting.php again

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -12,7 +12,7 @@
 // See https://api.drupal.org/api/drupal/sites!default!default.settings.php/8
 $databases = [];
 $config_directories = [];
-$settings['update_free_access'] = TRUE;
+$settings['update_free_access'] = FALSE;
 $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 $settings['file_scan_ignore_directories'] = [
   'node_modules',
